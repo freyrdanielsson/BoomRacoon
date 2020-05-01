f12bc55db7ec059fb51babf1e40b55fd26f59024
pgorgis <pgorgis@kth.se>
2020-05-01 18:27:10 +0200

    Added firebase, firestore + storage works, should be in redux

---
+++ src/firebase.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase';
+
+/*
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import 'firebase/storage';
+*/
+
+const config = {
+    apiKey: "AIzaSyDQHwzyIOmrS-d0GJJoOB78QOD9iKs1-T4",
+    authDomain: "dm2518-project-matching.firebaseapp.com",
+    databaseURL: "https://dm2518-project-matching.firebaseio.com",
+    projectId: "dm2518-project-matching",
+    storageBucket: "dm2518-project-matching.appspot.com",
+    messagingSenderId: "752684525992",
+    appId: "1:752684525992:web:7cec7ffa7210049f45159d",
+    measurementId: "G-Q7P4XSH56P"
+  };
+
+firebase.initializeApp(config);
+console.log("Initialized");
+
+export default firebase;
--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 
 import './Home.scss';
 
+import firebase from '../../firebase';
+
 export function Home(props) {
+
+    async function getCategories() {
+        const categories = await firebase.firestore().collection('categories').get()
+        return categories.docs.map(doc => {
+            console.log(doc.data());
+            doc.data();
+        });
+    }
+
+    async function getPic() {
+        const pic = firebase.storage().ref().child('test/doggydogg.jpg').getDownloadURL();
+        console.log(pic);
+    }
+
+    getCategories();
+    getPic();
+
+
+    
 
     return (
         <div className='home'>
